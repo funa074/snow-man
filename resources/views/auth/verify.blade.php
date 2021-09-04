c94a5119b2290e25f2fc284eb8d333cd3b29047b
funa074 <fnk074@gmail.com>
2021-09-05 05:17:17 +0900

    認証ページの内容を日本語に変更

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,21 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">本登録</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            メールが送信されました。
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                    メールを送信しました。メールからメールアドレスの認証をお願いします。<br>
+                    メールが届いていない場合は、<a href="{{ route('verification.resend') }}">ここをクリックしてください</a>。再送いたします。
                 </div>
             </div>
         </div>
