fae419d2bc875e0ddad596e1027891e4bb4a42d4
funa074 <fnk074@gmail.com>
2021-09-17 22:01:13 +0900

    トップページのUIを作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    <h1>❄️ SNOW MAN ❄️</h1>
-    <p>スキー場の積雪量などの情報を見たり、自分の滑走履歴を記録しよう！</p>
-    <a href="{{ route('register') }}">新規登録</a>
-    <a href="{{ route('login')}}">ログイン</a>
+    <div class=welcome-top>
+        <h1 class="welcome-title">❄️ SNOW MAN ❄️</h1>
+        <p class="welcome-text">
+            スキー場の天気や気温、積もっている雪の雪質や積雪量を確認したり<br>
+            自分の滑走履歴を記録しよう！
+        </p>
+    </div>
+
+    <div class="welcome-btn flex">
+        <a href="{{ route('register') }}" class="btn">新規登録</a>
+        <a href="{{ route('login')}}" class="btn">ログイン</a>
+    </div>
+
+    <div class="snow">●</div> 
 </body>
 </html>
