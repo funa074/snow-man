060d9baa20a06a7ea4111c1398c6eda2d4ea5c8c
funa074 <fnk074@gmail.com>
2021-09-25 13:14:08 +0900

    SkiRisortモデルのエラー解決 fix composer.json

--- app/Models/SkiResort.php
+++ app/Models/SkiResort.php
@@ -1,21 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class SkiResort extends Model
 {
-    protected $table = 'ski_resorts';
-    // protected $guarded = [
-    //     'id',
-    //     'created_at',
-    //     'updated_at'
-    // ];
     protected $fillable = [
-        'name',
-        'weather',
-        'temperature',
-        'snow_cover'
+        "weather",
+        "temperature",
+        "snow_cover",
     ];
 }
