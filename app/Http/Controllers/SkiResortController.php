d6851da8f9fd3c0e4847162b9c638ca0fc0c85b7
funa074 <fnk074@gmail.com>
2021-09-28 21:04:09 +0900

    メソッドインジェクションの削除

--- app/Http/Controllers/SkiResortController.php
+++ app/Http/Controllers/SkiResortController.php
@@ -10,7 +10,7 @@ use App\Models\SkiResort; // スキーリゾートモデルを使える様にし
 
 class SkiResortController extends Controller
 {
-    public function index(Request $request, $id) {
+    public function index($id) {
         $ski_resort = SkiResort::find($id); // 変数にDBのスキー場の情報を代入
         return view('ski_resort', compact('ski_resort'));
     } 
