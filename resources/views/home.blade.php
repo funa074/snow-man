9aec2be9df0cbc11fa7c6df096eb870bbb3cf3ce
funa074 <fnk074@gmail.com>
2021-09-28 20:09:22 +0900

    スキー場詳細ページ neme取得

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
 <h1 class="ski-resort-search">スキー場を探す</h1>
 <div class="ski-resort-list">
     @foreach($ski_resorts as $ski_resort)
-        <a href="{{ url('/ski-resort') }}" class="flex ski-resort">
+        <a href="/ski-resort/{{ $ski_resort->id }}" class="flex ski-resort">
             {{ $ski_resort->name }}
         </a>
         <br>
