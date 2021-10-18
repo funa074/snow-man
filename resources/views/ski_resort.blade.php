124999d4c66d1600078d95c336e12a45899c8458
funa074 <fnk074@gmail.com>
2021-10-18 23:54:49 +0900

    update ski_resort UI

--- resources/views/ski_resort.blade.php
+++ resources/views/ski_resort.blade.php
@@ -2,16 +2,19 @@
 
 @section('content')
 <h1 class="ski-resort">{{ $ski_resort->name }}</h1>
-<tr>
-  <td>天気</td>
-  <td>{{ $ski_resort_weather }}</td>
-</tr>
-<tr>
-  <td>気温</td>
-  <td>{{ $ski_resort_temp }}℃</td>
-</tr>
-<tr>
-  <td>積雪量</td>
-  <td>{{ $ski_resort_snow_cover }}cm</td>
-</tr>
+<table  class="ski-resort-table">
+  <tr>
+    <td>天気</td>
+    <td>{{ $ski_resort_weather }}</td>
+  </tr>
+  <tr>
+    <td>気温</td>
+    <td>{{ $ski_resort_temp }}℃</td>
+  </tr>
+  <tr>
+    <td>積雪量</td>
+    <td>{{ $ski_resort_snow_cover }}cm</td>
+  </tr>
+</table>
+<a href="/home" class="return">スキー場一覧に戻る</a>
 @endsection
