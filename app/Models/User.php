e18f738db0aa44bc53ddea4972d705df5def0622
funa074 <fnk074@gmail.com>
2021-10-23 22:02:28 +0900

    update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 use App\Notifications\ResetPassword;// 作ったResetPasswordをuseする
+use App\Models\Record;
 
 class User extends Authenticatable implements MustVerifyEmail // MustVerifyEmailをimplements
 {
