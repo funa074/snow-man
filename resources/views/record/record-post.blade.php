2d189a5ced5b462c40e15ddcd9ee9bcf36228dcd
funa074 <fnk074@gmail.com>
2021-10-28 21:27:38 +0900

    Update record-post UI

--- resources/views/record/record-post.blade.php
+++ resources/views/record/record-post.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="/recored-list" method="post">
+<form action="/record-create" method="post">
   @csrf
   <dl class="record flex">
 
     <dt><label class="label" for="date">滑走日</label></dt>
-    <dd><input class="inputs" id="date" type="date" required="required" name="date"></dd>
+    <dd><input class="inputs date" id="date" type="date" required="required" name="date"></dd>
   
     <dt><label class="label" for="ski-resort">スキー場名</label></dt>
-    <dd><input class="inputs" id="ski-resort" type="text" required="required" name="ski-resort"></dd>
+    <dd><input class="inputs text" id="ski-resort" type="text" required="required" name="ski-resort"></dd>
   
-    <dt><label class="label" for="text">本文</label></dt>
-    <dd><textarea class="inputs textarea" id="text"  required="required"></textarea></dd>
+    <dt><label class="label" for="text">本文</label><p class="any1">※任意</p></dt>
+    <dd><textarea class="inputs textarea" id="text"  required="required" name="body"></textarea></dd>
   
-    <dt class="flex"><label class="label" for="img">画像選択</label><p>※任意</p></dt>
+    <dt><label class="label" for="img">画像選択</label><p class="any2">※任意</p></dt>
     <dd><input type="file" name="img" id="img" accept=".png,.jpg,.jpeg,image/png,image/jpg"></dd>
   
     <input class="submit" type="submit" value="登録する">
