8c544201410e2f7399d0bc18d347371873f9f941
funa074 <fnk074@gmail.com>
2021-10-31 17:18:55 +0900

    Fix UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="ski-resort-search">スキー場を探す</h1>
+<h1 class="ski-resort-search">スキー場一覧</h1>
 <div class="ski-resort-list">
     @foreach($ski_resorts as $ski_resort)
         <a href="/ski-resort/{{ $ski_resort->id }}" class="flex ski-resorts">
