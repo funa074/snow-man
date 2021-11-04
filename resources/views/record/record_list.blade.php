cad4b40abb73a423587a9ba93a7f2754f887be6a
funa074 <fnk074@gmail.com>
2021-11-04 20:49:29 +0900

    Add record_list UI

--- resources/views/record/record_list.blade.php
+++ resources/views/record/record_list.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
 
-<div class="">
-    @foreach($record_values as $record_value)
-        <p>{{ $record_value->date }}</p>
-        <p>{{ $record_value->ski_resort }}</p>
-        <p>{{ $record_value->body }}</p>
-        <img src="storage/img/{{$record_value->image_file_name}}" alt="">
-        <br>
-    @endforeach
-</div>
+@foreach($record_values as $record_value)
+    <a href="/record" class="records-link">
+        <article class="records-list flex">
+            <img class="records-img" src="storage/img/{{$record_value->image_file_name}}" alt="">
+            <div class="records-table">
+                <p class="records-ski-resort">{{ $record_value->ski_resort }}</p>
+                <p class="records-date">{{ $record_value->date }}</p>
+                <p class="records-body">{{ $record_value->body }}</p>
+            </div>
+        </article>
+    </a>
+@endforeach
 
 @endsection
