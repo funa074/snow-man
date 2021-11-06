2425b935ea539e6a3c8c2aada2db717f5be3d321
funa074 <fnk074@gmail.com>
2021-11-06 12:30:55 +0900

    Fix layaut

--- resources/views/record/record_list.blade.php
+++ resources/views/record/record_list.blade.php
@@ -3,16 +3,18 @@
 @section('content')
 
 @foreach($record_values as $record_value)
-    <a href="/record" class="records-link">
-        <article class="records-list flex">
-            <img class="records-img" src="storage/img/{{$record_value->image_file_name}}" alt="">
+    <article class="records-list">
+        <a href="/record" class="records-link flex">
+            <div class="records-img-wrap">
+                <img class="records-img" src="storage/img/{{$record_value->image_file_name}}" alt="">
+            </div>
             <div class="records-table">
                 <p class="records-ski-resort">{{ $record_value->ski_resort }}</p>
                 <p class="records-date">{{ $record_value->date }}</p>
                 <p class="records-body">{{ $record_value->body }}</p>
             </div>
-        </article>
-    </a>
+        </a>
+    </article>
 @endforeach
 
 @endsection
