c2276f698254f7ede4eca956d03366552085205a
funa074 <fnk074@gmail.com>
2021-11-11 22:12:08 +0900

    Add JS to img-input

--- resources/views/records/record_edit.blade.php
+++ resources/views/records/record_edit.blade.php
@@ -14,12 +14,37 @@
   
     <dt><label class="label" for="text">本文</label><p class="any1">※任意</p></dt>
     <dd><textarea class="inputs textarea" id="text" name="body">{{ $record_values->body }}</textarea></dd>
-  
-    <dt><label class="label" for="img">画像選択</label><p class="any2">※任意</p></dt>
-    <dd><input type="file" name="img" id="img" accept=".png,.jpg,.jpeg,image/png,image/jpg"></dd>
-  
-    <input class="submit" type="submit" value="変更する">
+    
+    @if ( !empty( $record_values->image_file_name ))
+      <div class="record-img">
+        <img src="{{ asset("storage/img/".$record_values->image_file_name) }}" alt="" style="width: 300px">
+        <button id="img-change" class="img-change" type="button">画像を変更する</button>
+      </div>
+      
+      <div class="img-input-hidden">
+        <dt><label class="label" for="img">画像選択</label><p class="any2">※任意</p></dt>
+        <dd><input type="file" name="img" id="img" accept=".png,.jpg,.jpeg,image/png,image/jpg"></dd>
+      </div>
+    @else
+      <div class="img-input">
+        <dt><label class="label" for="img">画像選択</label><p class="any2">※任意</p></dt>
+        <dd><input type="file" name="img" id="img" accept=".png,.jpg,.jpeg,image/png,image/jpg"></dd>
+      </div>
+    @endif
+    <input class="edit-submit" type="submit" value="変更する">
 
   </dl>
 </form>    
 @endsection
+
+<script>
+  window.addEventListener("DOMContentLoaded", () => { // ページの読み込みが終わったら実行
+    const button = document.querySelector("#img-change");
+    button.addEventListener("click", () => {
+      const img = document.querySelector(".record-img");
+      img.classList.add("active");
+      const imgInput = document.querySelector(".img-input-hidden");
+      imgInput.classList.add("active");
+    })
+  })
+</script>
