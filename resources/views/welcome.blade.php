9532eb81c2ae47c9438ce24e7d0fbd938c3f5ab3
funa074 <fnk074@gmail.com>
2021-11-28 02:01:18 +0900

    Fix element centered

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,17 @@
     <title>{{ config('app.name') }}</title>
 </head>
 <body>
-    <h1 class="welcome-title">❄️ SNOW MAN ❄️</h1>
-    <p class="welcome-text">
-        ゲレンデの天気や気温、積もっている雪の量を確認したり<br>
-        自分の滑走履歴を記録しよう！
-    </p>
-
-    <div class="welcome-btn flex">
-        <a href="{{ route('register') }}" class="btn">新規登録</a>
-        <a href="{{ route('login') }}" class="btn">ログイン</a>
+    <div class="welcome-wrapper">
+        <h1 class="welcome-title">❄️ SNOW MAN ❄️</h1>
+        <p class="welcome-text">
+            ゲレンデの天気や気温、積もっている雪の量を確認したり<br>
+            自分の滑走履歴を記録しよう！
+        </p>
+    
+        <div class="welcome-btn flex">
+            <a href="{{ route('register') }}" class="btn">新規登録</a>
+            <a href="{{ route('login') }}" class="btn">ログイン</a>
+        </div>
     </div>
 
     <div class="snow">●</div> 
