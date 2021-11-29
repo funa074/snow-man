179bf0027d175b1d24a7fcf140b0b89ee9c213b2
funa074 <fnk074@gmail.com>
2021-11-29 23:41:46 +0900

    Add a message when there is no post

--- resources/views/records/list.blade.php
+++ resources/views/records/list.blade.php
@@ -2,19 +2,23 @@
 
 @section('content')
 
-@foreach($record_values as $record_value)
-    <article class="records-list">
-        <a href="/record/{{ $record_value->id }}" class="records-link flex">
-            <div class="records-img-wrap">
-                <img class="records-img" src="{{ asset($record_value->image_full_path) }}" />
-            </div>
-            <div class="records-table">
-                <p class="records-ski-resort">{{ $record_value->ski_resort }}</p>
-                <p class="records-date">{{ $record_value->date }}</p>
-                <p class="records-body">{{ $record_value->body }}</p>
-            </div>
-        </a>
-    </article>
-@endforeach
+@if($record_values->isNotEmpty())
+    @foreach($record_values as $record_value)
+        <article class="records-list">
+            <a href="/record/{{ $record_value->id }}" class="records-link flex">
+                <div class="records-img-wrap">
+                    <img class="records-img" src="{{ asset($record_value->image_full_path) }}" />
+                </div>
+                <div class="records-table">
+                    <p class="records-ski-resort">{{ $record_value->ski_resort }}</p>
+                    <p class="records-date">{{ $record_value->date }}</p>
+                    <p class="records-body">{{ $record_value->body }}</p>
+                </div>
+            </a>
+        </article>
+    @endforeach
+@else
+    <p class="text-center">まだ投稿がありません</p>
+@endif
 
 @endsection
