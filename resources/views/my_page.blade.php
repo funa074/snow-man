5ff874176b6e2179b96edd23773d550d6a42efae
funa074 <fnk074@gmail.com>
2021-11-30 23:15:38 +0900

    Fix element centered

--- resources/views/my_page.blade.php
+++ resources/views/my_page.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<table class="my-page-table">
-  <tr>
-    <td>ユーザー名</td>
-    <td>{{ $user->name }}</td>
-  </tr>
-  <tr>
-    <td>合計滑走回数</td>
-    <td>{{ $records->count() }}回</td>
-  </tr>
-  <tr>
-    <td>21-22シーズン滑走回数</td>
-    <td>{{ $season_records_count }}回</td>
-  </tr>
-</table>
-<div class="my-page-btn flex">
-  <a href="/record-list" class="btn">滑走履歴を確認</a>
-  <a href="/record-create" class="btn">滑走情報を登録</a>
+<div class="my-page-wrapper">
+  <table class="my-page-table">
+    <tr>
+      <td>ユーザー名</td>
+      <td>{{ $user->name }}</td>
+    </tr>
+    <tr>
+      <td>合計滑走回数</td>
+      <td>{{ $records->count() }}回</td>
+    </tr>
+    <tr>
+      <td>21-22シーズン滑走回数</td>
+      <td>{{ $season_records_count }}回</td>
+    </tr>
+  </table>
+  <div class="my-page-btn flex">
+    <a href="/record-list" class="btn">滑走履歴を確認</a>
+    <a href="/record-create" class="btn">滑走情報を登録</a>
+  </div>
+  <a href="/home" class="return">スキー場一覧に戻る</a>
 </div>
-<a href="/home" class="return">スキー場一覧に戻る</a>
-
 
 @endsection
--- resources/views/ski_resort.blade.php
+++ resources/views/ski_resort.blade.php
@@ -1,40 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="ski-resort">{{ $ski_resort->name }}</h1>
-<table  class="ski-resort-table">
-  <tr>
-    <td>現在の天気</td>
-    <td>
-      @if ($ski_resort_weather === 'Clear') 
-        晴れ
-      @elseif ($ski_resort_weather === 'Clouds')
-        くもり
-      @elseif ($ski_resort_weather === 'Rain')
-        雨
-      @elseif ($ski_resort_weather === 'Drizzle')
-        小雨  
-      @elseif ($ski_resort_weather === 'Thunderstorm')
-        雷雨
-      @elseif ($ski_resort_weather === 'Snow')
-        雪
-      @endif
-    </td>
-  </tr>
-  <tr>
-    <td>日中の気温</td>
-    <td>
-      @if ($ski_resort_temp === -0)
-        0℃
-      @else
-        {{ $ski_resort_temp }}℃
-      @endif
-    </td>
-  </tr>
-  <tr>
-    <td>積雪量</td>
-    <td>{{ $ski_resort_snow_cover }}cm</td>
-  </tr>
-</table>
-<a href="/home" class="return">スキー場一覧に戻る</a>
+<div class="ski-resort-wrapper">
+  <h1 class="ski-resort">{{ $ski_resort->name }}</h1>
+  <table  class="ski-resort-table">
+    <tr>
+      <td>現在の天気</td>
+      <td>
+        @if ($ski_resort_weather === 'Clear') 
+          晴れ
+        @elseif ($ski_resort_weather === 'Clouds')
+          くもり
+        @elseif ($ski_resort_weather === 'Rain')
+          雨
+        @elseif ($ski_resort_weather === 'Drizzle')
+          小雨  
+        @elseif ($ski_resort_weather === 'Thunderstorm')
+          雷雨
+        @elseif ($ski_resort_weather === 'Snow')
+          雪
+        @endif
+      </td>
+    </tr>
+    <tr>
+      <td>日中の気温</td>
+      <td>
+        @if ($ski_resort_temp === -0)
+          0℃
+        @else
+          {{ $ski_resort_temp }}℃
+        @endif
+      </td>
+    </tr>
+    <tr>
+      <td>積雪量</td>
+      <td>{{ $ski_resort_snow_cover }}cm</td>
+    </tr>
+  </table>
+  <a href="/home" class="return">スキー場一覧に戻る</a>
+</div>
 @endsection
