7f2af6ff4e7f2c5812ae70e9c8abc4c450f37cb8
funa074 <fnk074@gmail.com>
2021-12-02 23:13:04 +0900

    Update number of ski resort

--- database/seeds/SkiResortsSeeder.php
+++ database/seeds/SkiResortsSeeder.php
@@ -15,13 +15,6 @@ class SkiResortsTableSeeder extends Seeder
         DB::table('ski_resorts')->truncate(); //2回目実行の際にシーダー情報をクリア
         
         $ski_resort_value = new \App\Models\SkiResort([
-            'name' => '六甲山スノーパーク',
-            'lat'  => 34.7679067,
-            'lon'  => 135.2426071
-        ]);
-        $ski_resort_value->save();
-
-        $ski_resort_value = new \App\Models\SkiResort([
             'name' => 'めいほうスキー場',
             'lat'  => 35.941763, 
             'lon'  => 137.042953
@@ -55,7 +48,7 @@ class SkiResortsTableSeeder extends Seeder
             'lon'  => 136.934835
         ]);
         $ski_resort_value->save();
-
+        
         $ski_resort_value = new \App\Models\SkiResort([
             'name' => 'ウィングヒルズ白鳥リゾート',
             'lat'  => 35.9663236,
@@ -64,9 +57,58 @@ class SkiResortsTableSeeder extends Seeder
         $ski_resort_value->save();
 
         $ski_resort_value = new \App\Models\SkiResort([
+            'name' => 'ハチ北スキー場',
+            'lat'  => 35.40689,
+            'lon'  => 134.5485514
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => '琵琶湖バレイ',
+            'lat'  => 35.2085,
+            'lon'  => 135.8926
+        ]);
+        $ski_resort_value->save();
+        
+        $ski_resort_value = new \App\Models\SkiResort([
             'name' => '横手山・渋峠スキー場',
             'lat'  => 36.6701251,
             'lon'  => 138.5274677
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => 'グラン・ヒラフ',
+            'lat'  => 42.8648783,
+            'lon'  => 140.700983
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => 'スキージャム勝山',
+            'lat'  => 36.0602,
+            'lon'  => 136.6003
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => 'かぐらスキー場',
+            'lat'  => 36.8942267,
+            'lon'  => 138.7662481
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => '川場スキー場',
+            'lat'  => 36.760127,
+            'lon'  => 139.106217
+        ]);
+        $ski_resort_value->save();
+
+        $ski_resort_value = new \App\Models\SkiResort([
+            'name' => '安比高原スキー場',
+            'lat'  => 40.0022785,
+            'lon'  => 140.9706213
         ]);
         $ski_resort_value->save();
     }
